c5b14dd28d189dbbc63614d4ebeb9317631e57a7
Zack Atrash <ziad.natrash@gmail.com>
2025-05-21 15:09:44 +0200

    nothing

--- Backend/Backend/src/main/java/com/bankapp/Backend/config/SecurityConfig.java
+++ Backend/Backend/src/main/java/com/bankapp/Backend/config/SecurityConfig.java
@@ -61,5 +61,5 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
-
+    
 }

9ad0551fbb517fc6570f998cc00a2e23851ad08b
Zack Atrash <ziad.natrash@gmail.com>
2025-05-29 20:34:21 +0200

    small fix

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -7,7 +7,7 @@ const api = axios.create({
 api.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   
-  if (token && !config.url.includes('/login') && !config.url.includes('/user/register')) {
+  if (token && !config.url.includes('/login') && !config.url.includes('/register')) {
     config.headers.Authorization = `Bearer ${token}`;
   }
 

215995f0f88f512329d6740b3df9c8d4e1dc4857
Mahmoud Farid <mahmoudf_1993@hotmail.com>
2025-06-08 13:36:50 +0200

    router

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useAuthStore } from "../stores/authstore";
+
 import LoginPage from "../views/LoginPage.vue";
 import Atm from "../views/Atm.vue";
 import RegisterPage from "../views/RegisterPage.vue";
@@ -11,30 +12,27 @@ import DailyLimitChangeForm from "@/views/DailyLimitChangeForm.vue";
 import AccountInfoPage from "@/views/AccountInfoPage.vue";
 import AllAccountsPage from "@/views/AllAccountsPage.vue";
 import TransferFundsPage from "@/views/TransferFundsPage.vue";
-import ApproveCustomers from '../views/ApproveCustomers.vue';
-
+import ApproveCustomers from "@/views/ApproveCustomers.vue";
+import TransactionForm from "@/views/TransactionForm.vue";
 
 const routes = [
   { path: "/login", component: LoginPage },
   { path: "/register", component: RegisterPage },
-  { path: "/transaction", component: TransactionForm, meta: {requiresAuth: true} },
+  { path: "/transaction", component: TransactionForm, meta: { requiresAuth: true } },
   { path: "/transaction-history", component: UserTransactions, meta: { requiresAuth: true } },
-  { path: ""}
   { path: "/daily-limit", component: DailyLimitChangeForm, meta: { requiresAuth: true } },
-  {path: "/Atm", component: Atm },
-
+  { path: "/Atm", component: Atm },
   { path: "/test", component: TestPage, meta: { requiresAuth: true } },
   {
-    path: '/employee',
-    name: 'EmployeeDashboard',
+    path: "/employee",
+    name: "EmployeeDashboard",
     component: EmployeeDashboard,
-    meta: { requiresAuth: true, role: 'EMPLOYEE' }
+    meta: { requiresAuth: true, role: "EMPLOYEE" }
   },
-    { path: "/unauthorized", component: UnAuthorizedPage },
-    { path: "/account", component: AccountInfoPage },
-    { path: "/account/all", component: AllAccountsPage },
-    { path: "/transfer", component: TransferFundsPage },
-
+  { path: "/unauthorized", component: UnAuthorizedPage },
+  { path: "/account", component: AccountInfoPage },
+  { path: "/account/all", component: AllAccountsPage },
+  { path: "/transfer", component: TransferFundsPage },
 ];
 
 const router = createRouter({
@@ -46,15 +44,14 @@ router.beforeEach((to, from, next) => {
   const authStore = useAuthStore();
 
   if (to.meta.requiresAuth && !authStore.isAuthenticated) {
-    return next({ path: '/login', query: { redirect: to.fullPath } });
+    return next({ path: "/login", query: { redirect: to.fullPath } });
   }
 
   if (to.meta.role && authStore.role !== to.meta.role) {
-    return next('/unauthorized'); 
+    return next("/unauthorized");
   }
 
   next();
 });
-
 
 export default router;

fbbf281f4216e5da9ba49c46d41f7bca7074fd0f
Mahmoud Farid <mahmoudf_1993@hotmail.com>
2025-06-09 22:55:36 +0200

    Finalized Transaction form and daily and aboslute limit changes

---
+++ Backend/Backend/src/main/java/com/bankapp/Backend/exception/CustomerTransactionException.java
@@ -0,0 +1,7 @@
+package com.bankapp.Backend.exception;
+
+public class CustomerTransactionException extends RuntimeException {
+  public CustomerTransactionException(String message) {
+    super(message);
+  }
+}

888451cb358de0dd1c11878c1e1f807a2f796b2a
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-06-11 21:44:53 +0200

    try

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: '/Code-Generation/', // ← fix is here
+  base: '/', // ← fix is here
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

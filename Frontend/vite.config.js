387b1b4e34fa1803b407deb0d1229710e6b540c0
Zack Atrash <ziad.natrash@gmail.com>
2025-06-11 19:17:33 +0200

    small fix to vite

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: 'Code-Generation',
+  base: '/Code-Generation/', // ← fix is here
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))

72507d88295157569b9d0942b62a51e2c9746fe3
Zack Atrash <ziad.natrash@gmail.com>
2025-06-11 21:56:06 +0200

    added new frontend ulr to backend

--- Backend/Backend/src/main/java/com/bankapp/Backend/config/CorsConfig.java
+++ Backend/Backend/src/main/java/com/bankapp/Backend/config/CorsConfig.java
@@ -16,7 +16,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:5173")
+                        .allowedOrigins("https://inbanking.netlify.app/")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);

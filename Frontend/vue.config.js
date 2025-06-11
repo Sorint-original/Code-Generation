5d76a5b46ed928b8bce2ed62be5d489589d1fe25
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-06-11 17:21:46 +0200

    path

--- Frontend/vue.config.js
+++ Frontend/vue.config.js
@@ -4,5 +4,9 @@ module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
     port: 5173, // Boys I added this so the app runs on port 5173, if you look carefully at the number 5173, it is the word "site" splled in numerals 
-  }
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+      ? '/Code-Generation/'
+      : '/'
 });
+

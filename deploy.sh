41de68c5e3e43728e056299fa73386744023bac9
Sorint-original <85764494+Sorint-original@users.noreply.github.com>
2025-06-11 17:57:34 +0200

    Revert "added dist for deployment"
    This reverts commit 49f173d6b4dee5ce6d2b20a3703774a7a3a3b3d6.

--- deploy.sh
+++
@@ -1,9 +0,0 @@
-yaml
-# Same as above, but change these lines:
-      - name: Build project
-        run: npm run build
-
-      - name: Deploy to GH Pages
-        uses: peaceiris/actions-gh-pages@v3
-        with:
-          publish_dir: ./Frontend/dist

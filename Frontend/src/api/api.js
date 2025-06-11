10c6d6a987739f11db92a83ad19d2c13fef6c685
Zack Atrash <ziad.natrash@gmail.com>
2025-06-11 21:52:42 +0200

    frontend link with new backend

--- Frontend/src/api/api.js
+++ Frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api',
+  baseURL: 'https://code-generation-4swu.onrender.com/api',
 });
 
 api.interceptors.request.use((config) => {
